cd2bc237bb4589dcc157db4a6d8a77cd9aa27df5
Khairul <khairul.web1@gmail.com>
2025-03-02 15:41:13 +0600

    complete the all card button

--- js/index.js
+++ js/index.js
@@ -36,3 +36,79 @@ document.getElementById('btn-dark').addEventListener('click', function () {
     document.getElementById('btn-dark').setAttribute('disabled', true);
 })
 
+// optimize Home Page
+document.getElementById('btn-home').addEventListener('click', function () {
+    const task = document.getElementById('task');
+    task.innerText = task.innerText - 1;
+    const devBoard = document.getElementById('dev-board');
+    devBoard.innerText = +devBoard.innerText + 1;
+
+    const history = document.getElementById('history');
+    const head = document.getElementById('home-head').innerText;
+    const p = document.createElement('p');
+    p.classList.add('history');
+    p.innerText = 'You have complete The Task '+ head +' at '+ new Date().toLocaleString();
+    history.appendChild(p);
+
+    document.getElementById('btn-home').style.backgroundColor = 'gray';
+    alert('Board update successfully');
+    document.getElementById('btn-home').setAttribute('disabled', true);
+})
+
+// Add new emoji
+document.getElementById('btn-emoji').addEventListener('click', function () {
+    const task = document.getElementById('task');
+    task.innerText = task.innerText - 1;
+    const devBoard = document.getElementById('dev-board');
+    devBoard.innerText = +devBoard.innerText + 1;
+
+    const history = document.getElementById('history');
+    const head = document.getElementById('emoji-head').innerText;
+    const p = document.createElement('p');
+    p.classList.add('history');
+    p.innerText = 'You have complete The Task '+ head +' at '+ new Date().toLocaleString();
+    history.appendChild(p);
+
+    document.getElementById('btn-emoji').style.backgroundColor = 'gray';
+    alert('Board update successfully');
+    document.getElementById('btn-emoji').setAttribute('disabled', true);
+})
+
+// Integrate OpenAI API
+document.getElementById('btn-api').addEventListener('click', function () {
+    const task = document.getElementById('task');
+    task.innerText = task.innerText - 1;
+    const devBoard = document.getElementById('dev-board');
+    devBoard.innerText = +devBoard.innerText + 1;
+
+    const history = document.getElementById('history');
+    const head = document.getElementById('api-head').innerText;
+    const p = document.createElement('p');
+    p.classList.add('history');
+    p.innerText = 'You have complete The Task '+ head +' at '+ new Date().toLocaleString();
+    history.appendChild(p);
+
+    document.getElementById('btn-api').style.backgroundColor = 'gray';
+    alert('Board update successfully');
+    document.getElementById('btn-api').setAttribute('disabled', true);
+})
+
+//Improve job searching 
+document.getElementById('btn-job').addEventListener('click', function () {
+    const task = document.getElementById('task');
+    task.innerText = task.innerText - 1;
+    const devBoard = document.getElementById('dev-board');
+    devBoard.innerText = +devBoard.innerText + 1;
+
+    const history = document.getElementById('history');
+    const head = document.getElementById('job-head').innerText;
+    const p = document.createElement('p');
+    p.classList.add('history');
+    p.innerText = 'You have complete The Task '+ head +' at '+ new Date().toLocaleString();
+    history.appendChild(p);
+
+    document.getElementById('btn-job').style.backgroundColor = 'gray';
+    alert('Board update successfully');
+    document.getElementById('btn-job').setAttribute('disabled', true);
+})
+
