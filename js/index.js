2551c4ecafd8f6bd2a290b256145904318f1da6e
Khairul <khairul.web1@gmail.com>
2025-03-02 14:55:19 +0600

    commit dark mode

--- js/index.js
+++ js/index.js
@@ -16,3 +16,23 @@ document.getElementById('btn-mobile').addEventListener('click', function () {
     alert('Board update successfully');
     document.getElementById('btn-mobile').setAttribute('disabled', true);
 })
+
+// Add Dark Mode
+document.getElementById('btn-dark').addEventListener('click', function () {
+    const task = document.getElementById('task');
+    task.innerText = task.innerText - 1;
+    const devBoard = document.getElementById('dev-board');
+    devBoard.innerText = +devBoard.innerText + 1;
+
+    const history = document.getElementById('history');
+    const head = document.getElementById('dark-head').innerText;
+    const p = document.createElement('p');
+    p.classList.add('history');
+    p.innerText = 'You have complete The Task '+ head +' at '+ new Date().toLocaleString();
+    history.appendChild(p);
+
+    document.getElementById('btn-dark').style.backgroundColor = 'gray';
+    alert('Board update successfully');
+    document.getElementById('btn-dark').setAttribute('disabled', true);
+})
+
